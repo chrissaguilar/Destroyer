27afc61be8b1cb6eeb2be0d045de90c64be90499
Chris Aguilar <Chris@Chriss-MacBook-Pro.local>
2017-12-28 21:57:26 -0600

    hello world

--- Destroyer/ViewController.swift
+++ Destroyer/ViewController.swift
@@ -11,8 +11,10 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad();
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Hello World");
     }
 
     override func didReceiveMemoryWarning() {
